83032152db892e2a161a275f8d07bcf00049f68c
Maxime <maxime.germano@epfl.ch>
2022-06-17 11:36:09 +0200

    save to github

---
+++ backups/host_v3.cpp
@@ -0,0 +1,264 @@
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+#include "cmdlineparser.h"
+#include <iostream>
+#include <cstring>
+#include <chrono>
+
+#include <fcntl.h>
+#include <fstream>
+#include <iomanip>
+#include <iosfwd>
+#include <unistd.h>
+
+// XRT includes
+#include "experimental/xrt_bo.h"
+#include "experimental/xrt_device.h"
+#include "experimental/xrt_kernel.h"
+
+#define DATA_SIZE (2000000000/4)
+
+double throughput_from_fpga_max_host_to_ssd = 0;
+double throughput_from_fpga_max_ssd_to_host = 0;
+double throughput_from_cpu_max_host_to_ssd = 0;
+double throughput_from_cpu_max_ssd_to_host = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+class Timer {
+    std::chrono::high_resolution_clock::time_point mTimeStart;
+
+   public:
+    Timer() { reset(); }
+    long long stop() {
+        std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::seconds>(timeEnd - mTimeStart).count();
+    }
+    void reset() { mTimeStart = std::chrono::high_resolution_clock::now(); }
+};
+
+Timer global_timer;
+int size = DATA_SIZE;
+size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
+
+std::pair<double, double> p2p_host_to_ssd(int& nvmeFd, xrtDeviceHandle device, xrt::kernel& krnl, xrt::bo bo0, xrt::bo p2p_bo1, int* p2p_bo1_map) {
+	Timer timer_from_cpu, timer_from_fpga;
+    int ret = 0;
+
+
+    std::cout << "Start timer" << " : " << global_timer.stop() << std::endl;
+    timer_from_cpu = Timer();
+
+    // Synchronize input buffer data to device global memory
+    bo0.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+    // Execution of the kernel
+    auto run = krnl(bo0, p2p_bo1);
+    run.wait();
+
+    // Start timer
+    timer_from_fpga = Timer();
+
+    std::cout << "Now start P2P Write from device buffers to SSD" << " : " << global_timer.stop() << std::endl;
+    ret = pwrite(nvmeFd, (void*)p2p_bo1_map, vector_size_bytes, 0);
+    if (ret == -1) std::cout << "P2P: write() failed, err: " << ret << ", line: " << __LINE__ << std::endl;
+
+    std::cout << "Stop timers" << " : " << global_timer.stop() << std::endl;
+    long long duration_from_cpu = timer_from_cpu.stop();
+    long long duration_from_fpga = timer_from_fpga.stop();
+
+    std::cout << "Compute throughputs" << " : " << global_timer.stop() << std::endl;
+    double throughput = vector_size_bytes;
+    throughput /= 1024 * 1024; // convert to MB
+
+    double throughput_from_fpga = throughput / duration_from_fpga;
+    double throughput_from_cpu = throughput / duration_from_cpu;
+
+    if (throughput_from_fpga > throughput_from_fpga_max_host_to_ssd) {
+    	throughput_from_fpga_max_host_to_ssd = throughput_from_fpga;
+    }
+
+    if (throughput_from_cpu > throughput_from_cpu_max_host_to_ssd) {
+    	throughput_from_cpu_max_host_to_ssd = throughput_from_cpu;
+    }
+
+    return std::make_pair(throughput_from_fpga, throughput_from_cpu);
+}
+
+std::pair<double, double> p2p_ssd_to_host(int& nvmeFd, xrtDeviceHandle device, xrt::kernel& krnl, xrt::bo bo_out, xrt::bo p2p_bo0, int* p2p_bo0_map) {
+	Timer timer_from_cpu, timer_from_fpga;
+
+    std::cout << "Start timers" << " : " << global_timer.stop() << std::endl;
+    timer_from_cpu = Timer();
+    timer_from_fpga = Timer();
+
+    std::cout << "Now start P2P Read from SSD to device buffers" << " : " << global_timer.stop() << std::endl;
+    if (pread(nvmeFd, (void*)p2p_bo0_map, vector_size_bytes, 0) <= 0) {
+        std::cerr << "ERR: pread failed: "
+                  << " error: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Stop timer" << " : " << global_timer.stop() << std::endl;
+    long long duration_from_fpga = timer_from_fpga.stop();
+
+    double throughput = vector_size_bytes;
+    throughput /= 1024 * 1024; // convert to MB
+
+    double throughput_from_fpga = throughput / duration_from_fpga;
+
+    if (throughput_from_fpga > throughput_from_fpga_max_ssd_to_host) {
+    	throughput_from_fpga_max_ssd_to_host = throughput_from_fpga;
+    }
+
+    // Execution of the kernel
+    auto run1 = krnl(p2p_bo0, bo_out);
+    run1.wait();
+
+    std::cout << "Get the output data from the device" << " : " << global_timer.stop() << std::endl;
+    bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+    long long duration_from_cpu = timer_from_cpu.stop();
+    double throughput_from_cpu = throughput / duration_from_cpu;
+
+    if (throughput_from_cpu > throughput_from_cpu_max_ssd_to_host) {
+    	throughput_from_cpu_max_ssd_to_host = throughput_from_cpu;
+    }
+
+    return std::make_pair(throughput_from_fpga, throughput_from_cpu);
+}
+
+int main(int argc, char** argv) {
+    // Command Line Parser
+    sda::utils::CmdLineParser parser;
+
+    global_timer = Timer();
+
+    // Switches
+    //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
+    parser.addSwitch("--xclbin_file", "-x", "input binary file string", "");
+    parser.addSwitch("--device_id", "-d", "device index", "0");
+    parser.addSwitch("--iterations", "-i", "number of iterations", "1000000");
+    parser.addSwitch("--file_path", "-p", "file path string", "");
+    parser.parse(argc, argv);
+
+    // Read settings
+    std::string binaryFile = parser.value("xclbin_file");
+    int device_index = stoi(parser.value("device_id"));
+    int num_iter = stoi(parser.value("iterations"));
+    std::string filepath = parser.value("file_path");
+
+    if (argc < 5) {
+        parser.printHelp();
+        return EXIT_FAILURE;
+    }
+
+    Timer timer = Timer();
+
+    std::cout << "Open the device" << device_index << std::endl;
+    auto device = xrt::device(device_index);
+    std::cout << "Load the xclbin " << binaryFile << std::endl;
+    auto uuid = device.load_xclbin(binaryFile);
+
+    auto krnl = xrt::kernel(device, uuid, "dummy_kernel");
+
+
+
+
+
+    xrt::bo::flags flags = xrt::bo::flags::p2p;
+    auto bo0_write = xrt::bo(device, vector_size_bytes, krnl.group_id(0));
+    auto p2p_write_bo1 = xrt::bo(device, vector_size_bytes, flags, krnl.group_id(1));
+
+    std::cout << "Map the contents of the buffer object into host memory : " << global_timer.stop() << std::endl;
+    auto bo0_write_map = bo0_write.map<int*>();
+    auto p2p_write_bo1_map = p2p_write_bo1.map<int*>();
+    
+    std::cout << "Fill the map" << " : " << global_timer.stop() << std::endl;
+    std::fill(bo0_write_map, bo0_write_map + size, 1);
+    std::fill(p2p_write_bo1_map, p2p_write_bo1_map + size, 1);
+
+    std::cout << "Allocate Buffer in Global Memory : " << global_timer.stop() << std::endl;
+    auto p2p_read_bo0 = xrt::bo(device, vector_size_bytes, flags, krnl.group_id(0));
+    auto bo_read_out = xrt::bo(device, vector_size_bytes, krnl.group_id(1));
+
+    std::cout << "Map the contents of the buffer object into host memory : " << global_timer.stop() << std::endl;
+    auto p2p_read_bo0_map = p2p_read_bo0.map<int*>();
+    auto bo_read_out_map = bo_read_out.map<int*>();
+
+    std::cout << "Fill the map : " << global_timer.stop() << std::endl;
+    std::fill(p2p_read_bo0_map, p2p_read_bo0_map + size, 0);
+    std::fill(bo_read_out_map, bo_read_out_map + size, 0);
+
+
+    std::cout << "\nStarting " << num_iter << " iterations W/R\n";
+    int nvmeFd = -1;
+    double sum_write_throughput_from_fpga = 0;
+    double sum_read_throughput_from_fpga = 0;
+    double sum_write_throughput_from_cpu = 0;
+    double sum_read_throughput_from_cpu = 0;
+    for (int i=0; i<num_iter; i++) {
+    	std::cout << "Iteration " << i << " : " << global_timer.stop() << std::endl;
+        std::cout << "P2P transfer from host to SSD" << " : " << global_timer.stop() << std::endl;
+        // Get access to the NVMe SSD.
+        nvmeFd = open(filepath.c_str(), O_RDWR | O_DIRECT);
+        if (nvmeFd < 0) {
+            std::cerr << "ERROR: open " << filepath << "failed: " << std::endl;
+            return EXIT_FAILURE;
+        }
+        auto p1 = p2p_host_to_ssd(nvmeFd, device, krnl, bo0_write, p2p_write_bo1, p2p_write_bo1_map);
+        sum_write_throughput_from_fpga += p1.first;
+        sum_write_throughput_from_cpu += p1.second;
+        (void)close(nvmeFd);
+
+        std::cout << "P2P transfer from SSD to host" << " : " << global_timer.stop() << std::endl;
+        nvmeFd = open(filepath.c_str(), O_RDWR | O_DIRECT);
+        if (nvmeFd < 0) {
+            std::cerr << "ERROR: open " << filepath << "failed: " << std::endl;
+            return EXIT_FAILURE;
+        }
+        auto p2 = p2p_ssd_to_host(nvmeFd, device, krnl, bo_read_out, p2p_read_bo0, p2p_read_bo0_map);
+        sum_read_throughput_from_fpga += p2.first;
+        sum_read_throughput_from_cpu += p2.second;
+        (void)close(nvmeFd);
+    }
+
+    double average_write_throughput_from_fpga = sum_write_throughput_from_fpga / num_iter;
+    double average_read_throughput_from_fpga = sum_read_throughput_from_fpga / num_iter;
+    double average_write_throughput_from_cpu = sum_write_throughput_from_cpu / num_iter;
+    double average_read_throughput_from_cpu = sum_read_throughput_from_cpu / num_iter;
+
+    std::cout << "\nWrite bandwidth achieved :\n"
+    		  << "		Max throughput from cpu: " << throughput_from_cpu_max_host_to_ssd << " MB/s\n"
+              << "		Average throughput from cpu: " << average_write_throughput_from_cpu << " MB/s\n\n"
+    		  << "		Max throughput from fpga: " << throughput_from_fpga_max_host_to_ssd << " MB/s\n"
+              << "		Average throughput from fpga: " << average_write_throughput_from_fpga << " MB/s\n";
+
+    std::cout << "\nRead bandwidth achieved :\n"
+    		  << "		Max throughput from cpu: " << throughput_from_cpu_max_ssd_to_host << " MB/s\n"
+              << "		Average throughput from cpu: " << average_read_throughput_from_cpu << " MB/s\n\n"
+    		  << "		Max throughput from fpga: " << throughput_from_fpga_max_ssd_to_host << " MB/s\n"
+              << "		Average throughput from fpga: " << average_read_throughput_from_fpga << " MB/s\n";
+
+    long long duration = timer.stop();// convert us to s;   
+    long long min = duration / 60;
+    long long sec = duration % 60; 
+
+    std::cout << "\nTotal time: " << min << "m" << sec << "s\n";
+
+    std::cout << "\nFINISHED\n";
+    return 0;
+}
---
+++ report.md
@@ -0,0 +1,53 @@
+# Benchmarks
+
+## Without FPGA
+
+The first benchmark is done with FIO, it tests the strict capabilities of the SSD without use of the FPGA. The results and the commands used can be be found inside **nofgpa_fio_benchmarks.txt**. 
+
+Four benchmarks were done, random read/write, and sequential read/write.
+
+## With FPGA
+
+Here we are conducting a throughput benchmark using the FPGA as DMA.
+
+The code can be found inside **bin/benchmark.cpp** and can be run with 
+
+> `./bin/benchmark -x ./bin/empty_kernel.xclbin -p <file path on the smartssd> -i <number of iterations>`
+
+An empty kernel is used because the data doesn't need to be modified on the fpga logic, but a kernel is still needed because the buffers are defined with it.
+
+The SSD has an ext4 filesystem with an empty file on it.
+
+### From Host Memory
+
+Unfortunatly, the U.2 platform on the SmartSSD does not support host memory access from the kernel. All data needs to be copied to the FPGA's global memory before use. More information can be found here https://xilinx.github.io/XRT/master/html/hm.html and here https://xilinx.github.io/Vitis-Tutorials/2021-2/build/html/docs/Hardware_Acceleration/Feature_Tutorials/08-using-hostmem/README.html.
+
+### Code 
+
+We first start in main by opening the device and loading the binary of the kernel inside. We also define a 2GB buffer for writing. We define it with the p2p flags, p2p flags are used for data transfer between an FPGA and an NVMe device. We define a `buffer_map` to map the contents of the buffer object into host memory. We use the map to fill the buffer with the value 1. This is a long operation (~30s) since there is 2gb to fill. We do it in the main so that it is done only once. It cannot be skipped as `pwrite()` will not work without it.
+
+Then, we start the iterations, first is the WRITE operation with the call of `p2p_host_to_ssd()` with the buffer and buffer_map as arguments. Before calling the function, we open the file on the SSD.
+
+The member function `sync` on the buffer synchronizes the buffer data to the FPGA's global memory. 
+
+Then we use `pwrite()` from `unistd.h` to write the data inside the buffer on the SSD file from the buffer map. 
+
+We use the user defined `Timer` class to compute the throughput and return it to `main`. Two timers are used, one starts before the function `sync` to compute the throughput from CPU to SSD, the second after `sync` to compute the throughput from FPGA to SSD.
+
+The second part of the iteration is the call to `p2p_ssd_to_host()` which is very similar to `p2p_host_to_ssd()` but defines its own buffers. `pread()` is used on the buffer map instead of `pwrite()`.
+
+### Results afer 300 iterations
+
+Write bandwidth achieved :
+- Max throughput from cpu: 1603.93 MB/s
+- Average throughput from cpu: 1524.55 MB/s
+- Max throughput from fpga: 1603.99 MB/s
+- Average throughput from fpga: 1524.61 MB/s
+
+Read bandwidth achieved :
+- Max throughput from cpu: 2924.12 MB/s
+- Average throughput from cpu: 2231.27 MB/s
+- Max throughput from fpga: 2924.26 MB/s
+- Average throughput from fpga: 2231.35 MB/s
+
+Total time: 14m45s
