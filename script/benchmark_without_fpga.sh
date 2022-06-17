581f061eebb1dd42a6216512e1d8105d3f1f411a
Maxime <maxime.germano@epfl.ch>
2022-06-17 19:44:59 +0200

    update results

--- script/benchmark_without_fpga.sh
+++ script/benchmark_without_fpga.sh
@@ -3,12 +3,8 @@
 # Run with 'sudo', needs root access.
 # SSD needs to be unmounted.
 
-# Will run for 40mins, 10mins per benchmark.
+# Will run for 30mins, 15mins per benchmark.
 
-fio --name=rand-write --ioengine=libaio --iodepth=256 --rw=randwrite --bs=4k --direct=1 --size=100% --numjobs=12 --runtime=600 --filename=/dev/nvme0n1 --group_reporting=1
+fio --name=write --ioengine=libaio --iodepth=64 --rw=write --bs=1024k --direct=1 --size=100% --numjobs=12 --runtime=900 --filename=/dev/nvme0n1 --group_reporting=1
 
-fio --name=rand-read --ioengine=libaio --iodepth=256 --rw=randread --bs=4k --direct=1 --size=100% --numjobs=12 --runtime=600 --filename=/dev/nvme0n1 --group_reporting=1
-
-fio --name=seq-write --ioengine=libaio --iodepth=64 --rw=write --bs=1024k --direct=1 --size=100% --numjobs=12 --runtime=600 --filename=/dev/nvme0n1 --group_reporting=1
-
-fio --name=seq-read --ioengine=libaio --iodepth=64 --rw=read --bs=1024k --direct=1 --size=100% --numjobs=12 --runtime=600 --filename=/dev/nvme0n1 --group_reporting=1
+fio --name=read --ioengine=libaio --iodepth=64 --rw=read --bs=1024k --direct=1 --size=100% --numjobs=12 --runtime=900 --filename=/dev/nvme0n1 --group_reporting=1
